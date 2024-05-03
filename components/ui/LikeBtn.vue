74be3e953e9698ac24523ff5f24858196fb1e162
Franck <franckvienot7@gmail.com>
2024-05-03 17:44:40 +0200

    add like and bookark btn && adjust Nav and start building Post

---
+++ components/ui/LikeBtn.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const liked = ref(false)
+
+const toggleLike = () => {
+  liked.value = !liked.value
+}
+</script>
+<template>
+  <div class="my-2">
+    <Transition name="fade" mode="out-in">
+      <Icon
+        v-if="liked"
+        name="material-symbols:favorite-rounded"
+        class="w-8 h-8 text-red-500"
+        @click="toggleLike"
+      />
+      <Icon
+        v-else
+        name="material-symbols:favorite-outline-rounded"
+        class="w-8 h-8 dark:text-white text-gray-800"
+        @click="toggleLike"
+      />
+    </Transition>
+  </div>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="w-full my-4">
-    <h1 class="text-3xl">This is the main page</h1>
-    <div class="flex">
-      <button class="btn">This is a custom button</button>
-    </div>
+  <div class="flex flex-col justify-center w-full my-4">
+    <Post />
   </div>
 </template>
