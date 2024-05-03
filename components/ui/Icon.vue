f65cb7d4815d88915b078859231b42fd8cdd6517
Franck <franckvienot7@gmail.com>
2024-05-03 16:28:16 +0200

    add Icons component and props

---
+++ components/ui/Icon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+<template>
+  <Icon :name="props.name" class="w-6 h-6 mr-3" color="white" />
+</template>
