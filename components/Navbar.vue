1b381c53da7aaa935067a0ec43450cf34a28f06a
Franck <franckvienot7@gmail.com>
2024-05-03 14:01:33 +0200

    make change in default layout and Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 <template>
-  <aside class="hidden sm:flex flex-col w-1/4 bg-neutral-800 h-screen">
-    <UiColorMode />
+  <aside
+    class="hidden sm:flex flex-col w-1/6 bg-neutral-800 h-screen justify-evenly px-4"
+  >
+    <div><UiColorMode /></div>
+
+    <div>Les liens</div>
+    <div>Connexion du user</div>
   </aside>
 </template>
 <style scoped></style>
