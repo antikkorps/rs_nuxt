b050f67e27a9a95b0e5e64bc2071b579fe47b41e
Franck <franckvienot7@gmail.com>
2024-05-03 10:25:42 +0200

    add tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-icon"]
+  modules: ["@nuxt/ui"],
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "~/tailwind.config.js",
+    exposeConfig: {
+      level: 2,
+    },
+    config: {},
+    viewer: true,
+  },
 })
