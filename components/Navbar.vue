109e794612cd3a3b9afb5d7c4c231eba6f259000
Franck <franckvienot7@gmail.com>
2024-05-03 11:57:09 +0200

    start building ui

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <aside class="hidden sm:flex flex-col w-1/4 bg-neutral-800 h-screen">
+    <UiColorMode />
+  </aside>
+</template>
+<style scoped></style>
