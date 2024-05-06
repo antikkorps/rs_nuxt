04260990aac93274a34b6d75770efad499db4b0c
Karl <karl.mullr@gmail.com>
2024-05-06 11:07:48 +0200

    add card component layout, used for login et register starter page

---
+++ components/ui/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+    <!-- <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12"></div> -->
+    <UCard>
+      <slot name="header" />
+
+      <slot name="content" />
+
+      <slot name="footer" />
+    </UCard>
+  </div>
+</template>
