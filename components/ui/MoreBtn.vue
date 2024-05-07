8ab824fb42fdb23ced4ab20c38182f7fd0ae4313
Franck <franckvienot7@gmail.com>
2024-05-07 13:59:50 +0200

    add more button && share button + position it in Post

---
+++ components/ui/MoreBtn.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const toggleMoreMenu = () => {
+  console.log("More button clicked still have to implement the more functionality")
+}
+</script>
+<template>
+  <div class="flex items-center space-x-2">
+    <button
+      class="flex items-center justify-center w-8 h-8 rounded-full bg-gray-100 dark:bg-gray-800"
+      @click="toggleMoreMenu"
+    >
+      <Icon name="ic:baseline-more-vert" class="w-6 h-6 dark:text-white text-gray-800" />
+    </button>
+  </div>
+</template>
