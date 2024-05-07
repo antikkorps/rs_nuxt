75c9e3fe0450417d090feeaf595640356737a1d5
Franck <franckvienot7@gmail.com>
2024-05-07 15:10:36 +0200

    add menuOpen and Overlay to the card

--- components/ui/MoreBtn.vue
+++ components/ui/MoreBtn.vue
@@ -8,31 +8,46 @@ const toggleMoreMenu = () => {
 <template>
   <div class="flex items-center space-x-2">
     <button
-      class="flex items-center justify-center w-8 h-8 rounded-full bg-gray-100 dark:bg-gray-800"
+      class="flex items-center justify-center w-8 h-8 rounded-full bg-gray-100 dark:bg-neutral-600"
       @click="toggleMoreMenu"
     >
       <Icon name="ic:baseline-more-vert" class="w-6 h-6 dark:text-white text-gray-800" />
     </button>
-    <!-- SlideoverMenu -->
-    <USlideover v-model="isOpen">
-      <UCard
-        class="flex flex-col flex-1"
-        :ui="{
-          body: { base: 'flex-1' },
-          ring: '',
-          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        }"
-      >
-        <template #header>
-          <Placeholder class="h-8" />
-        </template>
-
-        <Placeholder class="h-full" />
-
-        <template #footer>
-          <Placeholder class="h-8" />
-        </template>
-      </UCard>
-    </USlideover>
+    <div
+      v-if="isOpen"
+      class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"
+      @click="isOpen = false"
+    ></div>
+    <div
+      v-if="isOpen"
+      class="fixed right-0 sm:right-36 bottom-0 mt-2 w-full sm:w-4/6 bg-white dark:bg-neutral-700 rounded-lg shadow-lg h- z-50"
+    >
+      <div class="flex flex-col justify-center p-2">
+        <div class="w-full flex flex-row justify-end items-center mt-2">
+          <UiIcon name="ion:md-close-circle-outline" @click="toggleMoreMenu" />
+        </div>
+        <button
+          class="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-neutral-600 rounded-lg"
+        >
+          <Icon name="ic:outline-share" class="w-6 h-6 dark:text-white text-gray-800" />
+          <span>Share</span>
+        </button>
+        <button
+          class="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-neutral-600 rounded-lg"
+        >
+          <Icon
+            name="ic:outline-bookmark"
+            class="w-6 h-6 dark:text-white text-gray-800"
+          />
+          <span>Bookmark</span>
+        </button>
+        <button
+          class="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-neutral-600 rounded-lg"
+        >
+          <Icon name="ic:outline-report" class="w-6 h-6 dark:text-white text-gray-800" />
+          <span>Report</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
