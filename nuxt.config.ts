836f9a58d837005f6f19e7b1f987115736207b8e
Franck <franckvienot7@gmail.com>
2024-05-17 17:57:52 +0200

    add authjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt"],
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@hebilicious/authjs-nuxt"],
   colorMode: {
     preference: "dark",
   },
@@ -12,4 +12,19 @@ export default defineNuxtConfig({
     viewer: true,
   },
   extends: ["nuxt-emoji"],
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET, // You can generate one with `openssl rand -base64 32`
+    },
+    // github: {
+    //   clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+    //   clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
+    // },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
+      },
+    },
+  },
 })
