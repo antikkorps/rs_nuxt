b93d03a4044cf9db377bdb13d94b61ffe8b58840
Franck <franckvienot7@gmail.com>
2024-05-25 20:18:07 +0200

    redirection on login and conditionnal rendering of buttons depending of session

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { z } from "zod"
 
+definePageMeta({ middleware: "guest-only", auth: { authenticatedRedirectTo: "/" } })
+
 const email = ref("")
 const password = ref("")
 
