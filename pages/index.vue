bb220acf4e406dcafdecfeb0e4ab61c1ab2c6015
Franck <franckvienot7@gmail.com>
2024-05-27 20:51:56 +0200

    start building route

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import { getAllPosts } from "~/services"
+const posts = ref([])
+
+onMounted(async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const data = await response.json()
+  posts.value = data
+
+  console.log("les posts", posts.value)
+})
+</script>
 <template>
-  <div class="flex justify-center mx-auto w-full">
-    <Post />
+  <div class="flex flex-col justify-center mx-auto w-full">
+    <Post v-for="(post, index) in posts" :key="index" :post="post" />
   </div>
 </template>
--- services/postService.ts
+++
@@ -1,8 +0,0 @@
-export const getAllPosts = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-  if (!response.ok) {
-    throw new Error("Failed to fetch posts")
-  }
-  const data = await response.json()
-  return data
-}
