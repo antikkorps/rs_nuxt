c03201152b5e79cc7ac6e5398d36efd24d08483f
Karl <karl.mullr@gmail.com>
2024-05-28 12:17:04 +0200

    Loginschema in separate file + Zod handler error in loginform + UiErrorMessage component

---
+++ components/ui/ErrorMessage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+    message: {
+        type: String,
+        default: "",
+    },
+})
+</script>
+
+<template>
+    <div className="bg-red-50 p-3 rounded-md flex items-center gap-x-2 text-sm text-red-700">
+      <Icon
+          name="tabler:alert-circle"
+          class="w-6 h-6 text-red-500"
+        />
+      <p>{{ props.message }}</p>
+    </div>
+</template>
