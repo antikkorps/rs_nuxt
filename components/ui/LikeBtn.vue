6fcb6545e54c2190a60cdf07ce68ce97d7a4abbe
Karl <karl.mullr@gmail.com>
2024-05-31 17:24:19 +0200

    post can be liked and unliked, need now to implement toast for info, cursor pointer, and optimistic hook/ for correction we need to pass user.id as string in schema.prisma and in the like.ts api route, cause i've added a parseInt to the user.id

--- components/ui/LikeBtn.vue
+++ components/ui/LikeBtn.vue
@@ -1,12 +1,50 @@
 <script setup lang="ts">
+
+const props = defineProps({
+    likedItemId: {
+        type: Number
+    },
+    likeType: {
+        type: String,
+        default: 'POST'
+    }
+})
+
+
 const liked = ref(false)
 
-const toggleLike = () => {
+const toggleLike = async () => {
   liked.value = !liked.value
+
+  const response = await fetch('/api/v1/like', {
+    method: 'POST',
+    body: JSON.stringify({
+      likedItemId: props.likedItemId,
+      likeType: props.likeType,
+    }),
+  })
+  const data = await response.json()
 }
+
+const checkIfILiked = async () => {
+  const response = await fetch(`/api/v1/like?likedItemId=${props.likedItemId}`, {
+    method: 'GET'
+  })
+  const data = await response.json()
+  if(data === true) {
+    liked.value = true
+  }
+
+}
+
+onMounted(() => {
+  checkIfILiked()
+})
+
 </script>
 <template>
   <div>
+    <p @click="checkIfILiked">TEST</p>
     <div class="my-2 mr-2">
       <Transition name="fade" mode="out-in">
         <Icon
