de1f1e4b7c65653af73e64bb434c1aab3c17c0fc
Franck <franckvienot7@gmail.com>
2024-06-05 17:39:24 +0200

    made some visual changes

--- components/ui/ProfileCard.vue
+++ components/ui/ProfileCard.vue
@@ -3,13 +3,19 @@ const { session } = useAuth()
 </script>
 <template>
   <div>
-    <div
-      class="bg-white dark:bg-neutral-900 w-full sm:w-4/5 flex flex-col justify-center"
-    >
-      <img
-        :src="session?.user?.avatar"
-        class="rounded-full max-w-none w-14 h-14 sm:w-24 sm:h-24 mt-10"
-      />
+    <div class="bg-white dark:bg-neutral-900 w-full flex flex-col justify-center">
+      <div class="bg-slate-300 sm:w-3/4"></div>
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold text-center mt-10">Profile</h1>
+        <h2 class="text-xl text-center mt-2">Welcome back, {{ session?.user?.name }}</h2>
+        <p class="text-center mt-2">Here is your profile</p>
+      </div>
+      <div class="flex justify-center mx-auto">
+        <img
+          :src="session?.user?.avatar"
+          class="rounded-full max-w-none w-14 h-14 sm:w-24 sm:h-24 mt-10"
+        />
+      </div>
     </div>
   </div>
 </template>
