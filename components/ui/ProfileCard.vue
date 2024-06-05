f73d9109ad8fdab6b74329c297fb1e9793b57caf
Franck <franckvienot7@gmail.com>
2024-06-05 17:29:46 +0200

    start building the profile card

---
+++ components/ui/ProfileCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { session } = useAuth()
+</script>
+<template>
+  <div>
+    <div
+      class="bg-white dark:bg-neutral-900 w-full sm:w-4/5 flex flex-col justify-center"
+    >
+      <img
+        :src="session?.user?.avatar"
+        class="rounded-full max-w-none w-14 h-14 sm:w-24 sm:h-24 mt-10"
+      />
+    </div>
+  </div>
+</template>
---
+++ pages/profile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <UiProfileCard />
+  </div>
+</template>
