1f08ba558a0b6df0daaa56478cc87057db627272
Franck <franckvienot7@gmail.com>
2024-06-22 21:59:47 +0200

    visual adjustment of close button in [id].vue

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import type { ExtendedPost } from "~/types/posts";
-import { commentServices, postServices } from "~/services";
-import type { CommentFormatedWithCommentLikes } from "~/types/types";
-const route = useRoute();
-const postStore = usePostStore();
+import type { ExtendedPost } from "~/types/posts"
+import { commentServices, postServices } from "~/services"
+import type { CommentFormatedWithCommentLikes } from "~/types/types"
+const route = useRoute()
+const postStore = usePostStore()
 
-const post = ref() as unknown as Ref<ExtendedPost | null>;
-const comments = ref([]) as Ref<CommentFormatedWithCommentLikes[] | undefined>;
+const post = ref() as unknown as Ref<ExtendedPost | null>
+const comments = ref([]) as Ref<CommentFormatedWithCommentLikes[] | undefined>
 
 const refreshComments = async () => {
   const postId = parseInt(route.params.id[0])
@@ -34,11 +34,13 @@ watch(
 
 <template>
   <div class="flex flex-col gap-5 items-center justify-center py-6">
-    <div>
-      <UButton to="/" variant="link" class="flex items-center gap-3">
-        <span><Icon name="icon-park-outline:arrow-left" /></span>
-        <span>Retour aux posts </span></UButton
-      >
+    <div class="w-full">
+      <UButton to="/" variant="link" class="flex items-center justify-center gap-3">
+        <Icon
+          name="material-symbols:cancel-outline-rounded"
+          class="w-10 h-10 dark:text-neutral-300 text-neutral-900"
+        />
+      </UButton>
     </div>
     <Post v-if="post" :post="post" :comments="comments" type="show" />
   </div>
