c8e9a6b2df1a5c3b26bfd64a89bd570648396200
Karl <karl.mullr@gmail.com>
2024-06-29 10:16:52 +0200

    we can now answer to comment, into comment, into comment. And come back to previous comment

--- components/ui/comments/RowFooter.vue
+++ components/ui/comments/RowFooter.vue
@@ -17,51 +17,95 @@ const props = defineProps({
     type: Object as PropType<ExtendedPost>,
     required: true,
   },
+  type: {
+    type: String,
+    required: false,
+    default: "index",
+  },
 });
 
 
+// here logic for the footer of the row
 const isLiked =
   props.comment.commentLikes && props.comment.commentLikes.length > 0
     ? true
     : false;
 
 
-const comments = ref([]);
+// the "répondre" button and differents behaviors if it's in the index or in the modal.
+// he redirect if it's in the index page, of open the modal if we're in the show.
 
+const modalComment = useModalCommentsStore();
+const modalSelectedComment = computed(() => modalComment.selectedComment);
+
+const onClickButton = async ({comment} : {comment?: CommentFormatedWithCommentLikes | null}) => {
+  if (props.type === "index") {
+    await navigateTo("/posts/" + props.post.id);
+  } 
+
+  if (props.type === "show" && comment) {
+    modalComment.setSelectedComment(comment)
+    openModal();
+  }
+
+  if(props.type === "modal" && comment) {
+    modalComment.setSelectedComment(comment)
+  }
+};
+
+
+// here logic for the modal
+const postStore = usePostStore();
+const comments = ref([]);
+const isOpen = ref(false);
+const isLoading = ref(false);
+
+// fetch the comments of the selected comment.
 const fetchComments = async (commentId: number) => {
   isLoading.value = true;
   const response = await commentServices.getChildrenByCommentId(commentId);
   comments.value = response;
-  console.log(response);
   isLoading.value = false;
 };
 
 
-
-
-// All the modal stuff, to probably put in another component
-const isOpen = ref(false);
-const isLoading = ref(false);
-watch(isOpen, async (newValue) => {
-  if (newValue) {
+// what's happen when we openModal
+const openModal = async () => {
+  if (props.type !== "modal") {
+    isOpen.value = true;
     await fetchComments(props.comment.id);
+  }
+};
+watch(modalSelectedComment, async (newComment) => {
+  if (newComment) {
+    await fetchComments(newComment.id);
   }
 });
 
-const openModal = async () => {
-  isOpen.value = true;
-};
-const postStore = usePostStore()
+// when postStore hasNewComment (boolean) value change, we fetch again the comments of the selected comment.
+// then we reset the value of the boolean.
 watch(
   () => postStore.hasNewComment,
   async (newVal) => {
     if (newVal) {
-      await fetchComments(props.comment.id)
+      await fetchComments(modalSelectedComment.value?.id || props.comment.id);
       // await refreshComments()
-      postStore.resetNewCommentFlag()
+      postStore.resetNewCommentFlag();
     }
   }
-)
+);
+
+const backButton = () => {
+  if(modalSelectedComment.value?.parent) {
+    modalComment.setSelectedComment(modalSelectedComment.value?.parent);
+  } else {
+    isOpen.value = false;
+    modalComment.resetSelectedComment()
+  }
+ 
+};
+
+
 </script>
 
 <template>
@@ -78,40 +122,70 @@ watch(
     </div>
     <UiEmojiPicker />
     <div class="flex items-center">
-      <UiCommentBtn :count="comment._count.children ?? 0" />
+      <button type="button" @click="onClickButton({comment: comment})">
+        <UiCommentBtn :count="comment._count.children ?? 0" />
+      </button>
+
       <button
+        type="button"
         class="mx-2 py-2 px-4 font-medium hover:bg-neutral-50 dark:hover:bg-neutral-700 rounded-lg"
         label="Open"
-        @click="openModal"
+        @click="onClickButton({comment: comment})"
       >
-        Répondre
+        Répondre {{ comment.id }}
       </button>
     </div>
 
     <!-- ALL the modal stuff, maybe to put in another component -->
     <UModal v-model="isOpen">
-      <div class="p-4">
+      <div>
         <div class="max-h-[60vh] overflow-y-auto mb-5">
-          <UiLoader v-if="isLoading" />
-          <div v-else>
-            
-            <UiCommentsRow
-              v-for="(comment, index) in comments"
-              :key="index"
-              :post="post"
-              :comment="comment"
+         
+          <div v-if="isLoading">
+            LOADING ...
+          </div>
+          <div v-else class="relative w-full">
+            <div
+              class="sticky inset-x-0 top-0 h-fit backdrop-blur-xl bg-white/10"
+            >
+              <div class="p-4">
+                <div>
+                  <UButton
+                  type="button"
+                  @click="backButton"
+                  variant="link"
+                  >Retour</UButton>  
+                </div>
+                <UiCommentsRow
+                  :post="post"
+                  :comment="modalSelectedComment || comment"
+                  type="modal"
+                  :userId="userId"
+                />
+                <div class="h-px bg-white w-full" />
+              </div>
+            </div>
+            <div class="p-4">
+              <UiCommentsRow
+                v-for="(comment, index) in comments"
+                :key="index"
+                :post="post"
+                :comment="comment"
+                :userId="userId"
+                type="modal"
+              />
+
+              <UiCommentsCreate
+              :postId="post.id"
               :userId="userId"
+              type="COMMENT"
+              buttonText="Répondre"
+              :parentId="modalSelectedComment?.id || comment.id"
             />
-          
+            </div>
+
           </div>
         </div>
-        <UiCommentsCreate
-          :postId="post.id"
-          :userId="userId"
-          type="COMMENT"
-          buttonText="Répondre"
-          :parentId="comment.id"
-        />
       </div>
     </UModal>
   </div>
