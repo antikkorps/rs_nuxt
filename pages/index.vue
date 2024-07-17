85f7fde52d6e0c957b6304b9f15c828a1ed82059
Franck <franckvienot7@gmail.com>
2024-07-17 16:45:06 +0200

    add navigation to the bottom of window to prevent each fetch to start on top

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,55 @@
 <script setup lang="ts">
-import { postServices } from "~/services";
-import type { ExtendedPost } from "~/types/posts";
+import { postServices } from "~/services"
+import type { ExtendedPost } from "~/types/posts"
 
-const posts = ref<ExtendedPost[]>([]);
-const page = ref(1);
-const totalPages = ref(1);
-const limit = 3;
-const loading = ref(false);
+const posts = ref<ExtendedPost[]>([])
+const page = ref(1)
+const totalPages = ref(1)
+const limit = 3
+const loading = ref(false)
 
 const fetchPosts = async () => {
-  loading.value = true;
+  loading.value = true
+  const savedScrollPosition = window.scrollY
+  console.log("position Y: ", savedScrollPosition)
   try {
-    const response = await postServices.getAllPosts(page.value, limit);
-    console.log("response", response);
-    posts.value.push(...response.posts);
-    totalPages.value = response.totalPages;  // Supposez que l'API renvoie le nombre total de pages
-    page.value++;
+    const response = await postServices.getAllPosts(page.value, limit)
+    console.log("response", response)
+    posts.value.push(...response.posts)
+    totalPages.value = response.totalPages
+    page.value++
+    await nextTick()
+    requestAnimationFrame(() => {
+      window.scrollTo(0, savedScrollPosition)
+    })
   } catch (error) {
-    console.error("Failed to fetch posts:", error);
+    console.error("Failed to fetch posts:", error)
   } finally {
-    loading.value = false;
+    loading.value = false
   }
-};
+}
 
 const handleScroll = () => {
   const bottomOfWindow =
-    window.innerHeight + window.scrollY >=
-    document.documentElement.scrollHeight - 10; // Ajustez la marge pour éviter les déclenchements prématurés
+    window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 10
   if (bottomOfWindow && !loading.value && page.value <= totalPages.value) {
-    
-    fetchPosts();
+    fetchPosts()
   }
-};
+}
 
 onMounted(async () => {
-  await fetchPosts();
-  window.addEventListener("scroll", handleScroll);
-});
+  await fetchPosts()
+  window.addEventListener("scroll", handleScroll)
+})
 
 onUnmounted(() => {
-  window.removeEventListener("scroll", handleScroll);
-});
-
-
+  window.removeEventListener("scroll", handleScroll)
+})
 </script>
 <template>
   <div class="flex flex-col justify-center mx-auto w-full gap-3">
     <Post
-    v-if="!loading"
+      v-if="!loading"
       v-for="(post, index) in posts"
       :key="index"
       :post="post"
